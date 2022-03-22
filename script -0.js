8557cf91c14ead8fcf4511720172f357b16322f9
Patrick PHILIPOT <patrick.philipot@gmail.com>
2022-03-22 14:10:18 +0100

    Canvas 2

--- script -0.js
+++ script -0.js
@@ -8,15 +8,15 @@ playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 4 ;
+let frameY = 0 ;
 let gameFrame = 0;
 const staggerFrames = 5;
 
 function animate() {
   ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame/staggerFrames) % 11;
-  frameX = spriteWidth * position;
-  ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  // ctx.drawImage(playerImage, 0, 0);
+  // ctx.drawImage(playerImage, sx, sy, sw, sh, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
   if (gameFrame % staggerFrames == 0 ) {
     if (frameX < 6)  frameX++;
     else frameX = 0;
