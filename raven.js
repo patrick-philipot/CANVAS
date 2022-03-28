246b3cbde435c6304bf0f612bee539f0ffaf7370
Patrick PHILIPOT <patrick.philipot@gmail.com>
2022-03-28 16:48:50 +0200

    raven-1

--- raven.js
+++ raven.js
@@ -12,20 +12,40 @@ let ravens = [];
 
 class Raven {
   constructor(){
-    this.width = 100;
-    this.height = 50;
+    this. spriteWidth = 271;
+    this. spriteHeight = 194;
+    this.sizeModifier = Math.random() * 0.6 + 0.4
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
     this.directionY = Math.random() * 5 - 2.5;
     this.markedForDFeletion = false;
+    this.image = new Image();
+    this.image.src = 'raven.png';
+    this.frame = 0;
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 50 + 50;
   }
-  update() {
+  update(deltatime) {
+    if (this.y < 0 || this.y > canvas.height - this.height){
+      this.directionY = -this.directionY;
+    }
     this.x -= this.directionX;
+    this.y += this.directionY;
     if (this.x < 0 - this.width) this.markedForDFeletion = true;
+    this.timeSinceFlap += deltatime;
+    if(this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
   }
   draw(){
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+    //ctx.strokeRect(this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.image,this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
   }
 }
 
@@ -41,7 +61,7 @@ function animate(timestamp) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
   }
-  [...ravens].forEach(object => object.update());
+  [...ravens].forEach(object => object.update(deltaTime));
   [...ravens].forEach(object => object.draw());
   ravens = ravens.filter(object => !object.markedForDFeletion)
 
